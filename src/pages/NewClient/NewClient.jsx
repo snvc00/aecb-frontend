bbfd6db1954e5cd333fb98e1593b2855c910278d
Santiago Valle <snvc00@gmail.com>
2021-06-01 13:21:21 -0500

    Implement register form for clients

--- src/pages/NewClient/NewClient.jsx
+++ src/pages/NewClient/NewClient.jsx
@@ -7,6 +7,8 @@ import {
     Button,
     Breadcrumb,
     BreadcrumbItem,
+    InlineNotification,
+    NotificationActionButton,
 } from "carbon-components-react";
 
 import Header from "../../components/Header";
@@ -15,7 +17,7 @@ import ClientGeneralDataInput from "../../components/ClientGeneralDataInput";
 import ClientAddressInput from "../../components/ClientAddressInput";
 
 import { Helmet } from "react-helmet";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import "./NewClient.css";
 
@@ -24,24 +26,56 @@ const gridStyles = {
     marginTop: "80px"
 }
 
-const getClientData = (generalData, address) => {
-    const clientGeneralData = generalData.current.getGeneralData();
-    const clientAddress = address.current.getAddress();
-
-    if (clientGeneralData && clientAddress) {
-        return { ...clientGeneralData, ...clientAddress };
-    } 
-}
-
-const NewClient = () => { 
+const NewClient = () => {
     const generalDataRef = useRef();
     const addressRef = useRef();
+    const tokenRef = useRef();
+    const [showNotification, setShowNotification] = useState(false);
+    const [notificationInfo, setNotificationInfo] = useState({});
 
-    const submitClientRegister = () => {
-        const clientData = getClientData(generalDataRef, addressRef);
+    const getClientData = () => {
+        const clientGeneralData = generalDataRef.current.getGeneralData();
+        const clientAddress = addressRef.current.getAddress();
 
-        // Enviar datos a la API
-        console.log(clientData);
+        return { ...clientGeneralData, ...clientAddress, is_active: true, password: "secure" };
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const clientData = getClientData();
+
+        const response = await fetch("http://localhost:5000/api/clients", {
+            method: "POST",
+            body: JSON.stringify(clientData),
+            headers: {
+                "Content-Type": "application/json",
+                "Token": tokenRef.current.value,
+            },
+        }).catch(error => console.log(error));
+
+        const data = response !== undefined ? await response.json() : { error: { message: "Ha ocurrido un error" } };
+        updateNotification(data, e);
+    }
+
+    const updateNotification = (data, e) => {
+        const errorInData = data.hasOwnProperty("error");
+
+        const kind = errorInData ? "error" : "success";
+        const title = errorInData ? data.error.message : "Cliente registrado";
+
+        console.log(errorInData, kind, title);
+
+        if (!errorInData) {
+            e.target.reset();
+        }
+
+        setShowNotification(true);
+        setNotificationInfo({
+            kind: kind,
+            title: title,
+        });
+
+        window.scrollTo(0, 0);
     }
 
     return (
@@ -59,9 +93,29 @@ const NewClient = () => {
                             <BreadcrumbItem href="/clientes/registro" isCurrentPage>
                                 Registrar
                             </BreadcrumbItem>
-                        </Breadcrumb><br/><br/>
+                        </Breadcrumb><br /><br />
                         <MainHeading>Nuevo Cliente</MainHeading>
-                        <Form onSubmit={submitClientRegister}>
+                        {
+                            showNotification ?
+
+                                <InlineNotification
+                                    kind={notificationInfo.kind}
+                                    title={notificationInfo.title}
+                                    onCloseButtonClick={() => { setShowNotification(false); }}
+                                    actions={
+                                        <NotificationActionButton
+                                            onClick={() => { window.location.href = "/clientes/registrados" }}
+                                        >
+                                            Ver Clientes
+                                        </NotificationActionButton>
+                                    }
+                                />
+
+                                :
+
+                                <></>
+                        }
+                        <Form onSubmit={handleSubmit}>
                             <ClientGeneralDataInput ref={generalDataRef} />
                             <ClientAddressInput ref={addressRef} />
                             <TextInput
@@ -69,8 +123,10 @@ const NewClient = () => {
                                 labelText="Token de Administrador"
                                 size="lg"
                                 type="password"
+                                ref={tokenRef}
+                                required
                             />
-                            <Button onClick={submitClientRegister} >
+                            <Button type="submit" >
                                 Registrar
                             </Button>
                         </Form>
