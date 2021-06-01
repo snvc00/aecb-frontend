3262f83562027be60d2c4af7e540e75a4baad532
Santiago Valle <snvc00@gmail.com>
2021-06-01 13:21:40 -0500

    Implement register form for credit cards

--- src/pages/NewCreditCard/NewCreditCard.jsx
+++ src/pages/NewCreditCard/NewCreditCard.jsx
@@ -7,6 +7,8 @@ import {
     Button,
     Breadcrumb,
     BreadcrumbItem,
+    InlineNotification,
+    NotificationActionButton,
 } from "carbon-components-react";
 
 import Header from "../../components/Header";
@@ -15,28 +17,70 @@ import CreditCardGenericInput from "../../components/CreditCardGenericInput";
 import CreditCardSpecificsInput from "../../components/CreditCardSpecificsInput";
 
 import { Helmet } from "react-helmet";
-import { createRef } from "react";
+import { createRef, useRef, useState } from "react";
 
 const gridStyles = {
     maxWidth: "50rem",
     marginTop: "80px"
 }
 
-const getCreditCardData = (generics, specifics) => {
-    const creditCardGenerics = generics.current.getCreditCardGenerics();
-    const creditCardSpecifics = specifics.current.getCreditCardSpecifics();
-
-    if (creditCardGenerics && creditCardSpecifics) {
-        return { ...creditCardGenerics, ...creditCardSpecifics };
-    } 
-}
-
 const NewCreditCard = () => { 
     const creditCardGenericsRef = createRef();
     const creditCardSpecificsRef = createRef();
+    const tokenRef = useRef();
 
-    const handleOnSubmit = () => {
-        const creditCard = getCreditCardData(creditCardGenericsRef, creditCardSpecificsRef);
+    const [showNotification, setShowNotification] = useState();
+    const [notificationInfo, setNotificationInfo] = useState();
+
+    const getCreditCardData = () => {
+        const creditCardGenerics = creditCardGenericsRef.current.getCreditCardGenerics();
+        const creditCardSpecifics = creditCardSpecificsRef.current.getCreditCardSpecifics();
+    
+        if (creditCardGenerics && creditCardSpecifics) {
+            return { ...creditCardGenerics, ...creditCardSpecifics };
+        } 
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const creditCard = getCreditCardData();
+
+        await fetch("http://localhost:5000/api/cards", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Token": tokenRef.current.value
+            },
+            body: JSON.stringify(creditCard),
+        })
+            .then(body => body.json())
+            .then(data => {
+                if (data.hasOwnProperty("error")) {
+                    console.log(data.error);
+                    setNotificationInfo({
+                        kind: "error",
+                        title: data.error.message,
+                    });
+                    setShowNotification(true);
+                }
+                else {
+                    setNotificationInfo({
+                        kind: "success",
+                        title: "Tarjeta de crédito registrada",
+                    });
+                    setShowNotification(true);
+                    e.target.reset();
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                setNotificationInfo({
+                    kind: "error",
+                    title: "Ha ocurrido un error",
+                });
+                setShowNotification(true);
+            });
 
         console.log(creditCard);
     }
@@ -53,12 +97,32 @@ const NewCreditCard = () => {
                         <Breadcrumb>
                             <BreadcrumbItem href="/">Inicio</BreadcrumbItem>
                             <BreadcrumbItem href="/tarjetas">Tarjetas</BreadcrumbItem>
-                            <BreadcrumbItem href="/tartjetas/registrar" isCurrentPage>
+                            <BreadcrumbItem href="/tarjetas/registrar" isCurrentPage>
                                 Registrar
                             </BreadcrumbItem>
                         </Breadcrumb><br/><br/>
                         <MainHeading>Nueva Tarjeta de Crédito</MainHeading>
-                        <Form onSubmit={handleOnSubmit}>
+                        {
+                            showNotification ?
+
+                                <InlineNotification
+                                    kind={notificationInfo.kind}
+                                    title={notificationInfo.title}
+                                    onCloseButtonClick={() => { setShowNotification(false); }}
+                                    actions={
+                                        <NotificationActionButton
+                                            onClick={() => { window.location.href = "/tarjetas/registradas" }}
+                                        >
+                                            Ver Tarjeras de Crédito
+                                        </NotificationActionButton>
+                                    }
+                                />
+
+                                :
+
+                                <></>
+                        }
+                        <Form onSubmit={handleSubmit}>
                             <CreditCardGenericInput ref={creditCardGenericsRef} />
                             <CreditCardSpecificsInput ref={creditCardSpecificsRef} />
                             <TextInput
@@ -66,8 +130,10 @@ const NewCreditCard = () => {
                                 labelText="Token de Administrador"
                                 size="lg"
                                 type="password"
+                                ref={tokenRef}
+                                required
                             />
-                            <Button onClick={handleOnSubmit} >
+                            <Button type="submit">
                                 Añadir
                             </Button>
                         </Form>
