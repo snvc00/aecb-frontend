ff3ab5228e8f5fce904740c20c4f411bc4e0e990
Santiago Valle <snvc00@gmail.com>
2021-06-17 00:06:12 -0500

    Implement login features in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,42 @@ import {
     HeaderMenu,
     HeaderSideNavItems,
     SideNav,
-    SideNavItems
+    SideNavItems,
+    HeaderGlobalBar,
+    HeaderGlobalAction
 } from "carbon-components-react";
+import {
+    Login20 as Login,
+    Logout20 as Logout,
+} from "@carbon/icons-react";
+import { AuthContext } from "../../Auth";
+import { useContext } from "react";
+import app from "../../firebase";
 
 const Header = () => {
+    const { currentUser } = useContext(AuthContext);
+
+    const getPostfix = () => {
+        if (!currentUser) {
+            return "";
+        }
+
+        const domain = currentUser.email.split("@");
+        return domain[1] === "alumnos.udg.mx" ? "Administrativos" : "Clientes";
+    }
+
+    const headerNamePostfix = getPostfix();
+
+    const handleSession = () => {
+        if (currentUser) {
+            app.auth().signOut();
+            sessionStorage.clear();
+        }
+        else {
+            window.location.replace("/login");
+        }
+    }
+
     return (
         <HeaderContainer
             render={({ isSideNavExpanded, onClickSideNavExpand }) => (
@@ -24,21 +56,40 @@ const Header = () => {
                         isActive={isSideNavExpanded}
                     />
                     <HeaderName href="/" prefix="Banco Nacional">
-                        AECB
+                        AECB {headerNamePostfix}
                     </HeaderName>
+
                     <HeaderNavigation aria-label="Banco Nacional AECB">
                         <HeaderMenu aria-label="Clientes" menuLinkName="Clientes">
-                            {/*isCurrentPage*/}
                             <HeaderMenuItem href="/preaprobacion">Solicitar Preaprobación</HeaderMenuItem>
                             <HeaderMenuItem href="/preaprobacion/historial">Historial de Solicitudes</HeaderMenuItem>
                         </HeaderMenu>
                         <HeaderMenu aria-label="Administradores" menuLinkName="Administradores">
                             <HeaderMenuItem href="/clientes">Clientes</HeaderMenuItem>
                             <HeaderMenuItem href="/tarjetas">Tarjetas de Crédito</HeaderMenuItem>
-                            <HeaderMenuItem href="/reportes">Reportes</HeaderMenuItem>
+                            <HeaderMenuItem href="/promociones">Promociones</HeaderMenuItem>
+                            <HeaderMenuItem href="/seguros">Seguros</HeaderMenuItem>
+                            <HeaderMenuItem href="/clientes/solicitudes">Solicitudes</HeaderMenuItem>
                         </HeaderMenu>
-                        <HeaderMenuItem href="/beneficios">Nuestros Beneficios</HeaderMenuItem>
+                        <HeaderMenuItem href="/promociones-tarjetas">Promociones</HeaderMenuItem>
+                        <HeaderMenuItem href="/seguros-tarjetas">Seguros</HeaderMenuItem>
                     </HeaderNavigation>
+
+                    <HeaderGlobalBar>
+                        <HeaderGlobalAction
+                            aria-label={currentUser ? "Cerrar Sesión" : "Iniciar Sesión"}
+                            tooltipAlignment="end"
+                            onClick={handleSession}
+                        >
+                            {
+                                currentUser ?
+                                    <Logout />
+                                    :
+                                    <Login />
+                            }
+                        </HeaderGlobalAction>
+                    </HeaderGlobalBar>
+
                     <SideNav
                         aria-label="Navegación lateral"
                         expanded={isSideNavExpanded}
@@ -53,9 +104,12 @@ const Header = () => {
                                 <HeaderMenu aria-label="Administradores" menuLinkName="Administradores">
                                     <HeaderMenuItem href="/clientes">Clientes</HeaderMenuItem>
                                     <HeaderMenuItem href="/tarjetas">Tarjetas de Crédito</HeaderMenuItem>
-                                    <HeaderMenuItem href="/reportes">Reportes</HeaderMenuItem>
+                                    <HeaderMenuItem href="/promociones">Promociones</HeaderMenuItem>
+                                    <HeaderMenuItem href="/seguros">Seguros</HeaderMenuItem>
+                                    <HeaderMenuItem href="/clientes/solicitudes">Solicitudes</HeaderMenuItem>
                                 </HeaderMenu>
-                                <HeaderMenuItem href="/beneficios">Nuestros Beneficios</HeaderMenuItem>
+                                <HeaderMenuItem href="/promociones-tarjetas">Promociones</HeaderMenuItem>
+                                <HeaderMenuItem href="/seguros-tarjetas">Seguros</HeaderMenuItem>
                             </HeaderSideNavItems>
                         </SideNavItems>
                     </SideNav>
