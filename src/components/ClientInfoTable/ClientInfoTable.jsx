0332fc5f60cdbf528a72c5efcd60cd05d9c0607b
Santiago Valle <snvc00@gmail.com>
2021-06-17 00:08:03 -0500

    Add pagination, implement batch actions

--- src/components/ClientInfoTable/ClientInfoTable.jsx
+++ src/components/ClientInfoTable/ClientInfoTable.jsx
@@ -16,6 +16,7 @@ import {
     TableSelectRow,
     InlineNotification,
     NotificationActionButton,
+    Pagination,
 } from "carbon-components-react";
 
 import {
@@ -23,21 +24,23 @@ import {
     UserActivity24 as UserActivity,
 } from "@carbon/icons-react";
 
-import { Fragment } from "react";
+import { Fragment, useContext, useRef } from "react";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../Auth";
 
 const headers = [
-    {
-        key: "id",
-        header: "ID",
-    },
     {
         key: "name",
         header: "Nombre",
     },
     {
-        key: "password",
-        header: "Contraseña",
+        key: "id",
+        header: "Correo",
+    },
+    {
+        key: "last_update",
+        header: "Última Actualización",
     },
     {
         key: "curp",
@@ -52,7 +55,7 @@ const headers = [
         header: "RFC",
     },
     {
-        key: "monthly_income",
+        key: "income",
         header: "Ingreso Mensual",
     },
     {
@@ -72,8 +75,12 @@ const headers = [
         header: "Estado",
     },
     {
-        key: "is_active",
+        key: "active",
         header: "Actividad",
+    },
+    {
+        key: "has_credit",
+        header: "Tiene Crédito",
     },
 ];
 
@@ -86,57 +93,64 @@ var translationKeys = {
 const ClientInfoTable = (props) => {
     const [showNotification, setShowNotification] = useState(false);
     const [notificationInfo, setNotificationInfo] = useState({});
-
-    const handleBatchActionClickUpdateStatus = async (selectedRows) => {
-        console.log(selectedRows);
-        console.log("handleBatchActionClickUpdateStatus");
-        const token = sessionStorage.getItem("token") || "";
-        const curp = selectedRows[0].cells[3].value;
+    const [itemBegin, setItemBegin] = useState(0);
+    const [itemEnd, setItemEnd] = useState(10);
+    let history = useHistory();
+    const { currentUser } = useContext(AuthContext);
+ 
+    const handleBatchActionClickUpdateStatus = (selectedRows) => {
+        const token = sessionStorage.getItem("token");
+        const id = selectedRows[0].cells[1].value;
         const isActive = selectedRows[0].cells[11].value === "Activo";
 
-
-        await fetch(`http://localhost:5000/api/clients/${curp}`, {
+        fetch(`${process.env.REACT_APP_BACKEND_API}/api/clients/${id}/`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
                 "Token": token
             },
-            body: JSON.stringify({ is_active: !isActive }),
+            body: JSON.stringify({ active: !isActive }),
         })
-            .then(body => body.json())
-            .then(data => {
-                if (data.hasOwnProperty("error")) {
-                    console.log(data.error);
-                    setNotificationInfo({
-                        kind: "error",
-                        title: data.error.message,
-                    });
-                    setShowNotification(true);
-                }
-                else {
-                    setNotificationInfo({
-                        kind: "success",
-                        title: "Actividad modificada",
-                    });
-                    setShowNotification(true);
-                }
-            })
-            .catch(error => {
-                console.log(error);
+        .then(async (response) => ({ data: await response.json(), responseOk: response.ok }))
+        .then(({ data, responseOk }) => {
+            if (responseOk) {
                 setNotificationInfo({
-                    kind: "error",
-                    title: "Ha ocurrido un error",
+                    kind: "success",
+                    title: "Actividad modificada",
                 });
                 setShowNotification(true);
+            }
+            else {
+                throw data.detail;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            setNotificationInfo({
+                kind: "error",
+                title: error,
             });
+            setShowNotification(true);
+        });
     }
 
     const handleBatchActionClickUpdateAddress = selectedRows => {
-        console.log(selectedRows);
-        console.log("handleBatchActionClickUpdateAddress");
-        const curp = selectedRows[0].cells[3].value;
+        const id = selectedRows[0].cells[1].value;
 
-        window.location.replace(`/clientes/actualizar/${curp}`);
+        const location = {
+            pathname: `/clientes/actualizar/${id}`,
+            state: { 
+                client: { 
+                    curp: selectedRows[0].cells[3].value, 
+                    address: selectedRows[0].cells[7].value, 
+                    neighborhood: selectedRows[0].cells[8].value, 
+                    city: selectedRows[0].cells[9].value, 
+                    state: selectedRows[0].cells[10].value 
+                } 
+            }
+        }
+
+       history.push(location);
     }
 
     const customTranslationForTableBatchActions = (id, state) => {
@@ -146,94 +160,118 @@ const ClientInfoTable = (props) => {
         return `${state.totalSelected} ${translationKeys[id]}`;
     }
 
+    const handlePageChange = pagination => {
+        setItemBegin(pagination.pageSize * (pagination.page - 1));
+        setItemEnd(pagination.page * pagination.pageSize);
+    }
+
     return (
         <>
-        {showNotification ? 
-            <InlineNotification
-                kind={notificationInfo.kind || "error"}
-                title={notificationInfo.title || ""}
-                style={{ marginBottom: "2rem" }}
-                actions={
-                    <NotificationActionButton
-                        onClick={() => { window.location.reload() }}
-                    >
-                        Intentar de nuevo
-                    </NotificationActionButton>
-                }
-            >
-            </InlineNotification> 
-            
-            : 
-            
-            <></>
-        }
-        <DataTable
-            rows={props.clients}
-            headers={headers}
-            {...props}
-            render={({
-                rows,
-                headers,
-                getHeaderProps,
-                getSelectionProps,
-                getToolbarProps,
-                getBatchActionProps,
-                getRowProps,
-                onInputChange,
-                selectedRows,
-                getTableProps,
-                getTableContainerProps,
-            }) => (
-                <TableContainer
-                    title="Clientes Registrados"
-                    description="Selecciona un cliente para modificar su estado de actividad o domicilio."
-                    {...getTableContainerProps()}>
-                    <TableToolbar {...getToolbarProps()}>
-                        <TableBatchActions {...getBatchActionProps()} translateWithId={customTranslationForTableBatchActions}>
-                            <TableBatchAction
-                                renderIcon={UserActivity}
-                                iconDescription="Cambiar Estado de Actividad"
-                                onClick={() => {handleBatchActionClickUpdateStatus(selectedRows)}}>
-                                Cambiar Estado de Actividad
-                            </TableBatchAction>
-                            <TableBatchAction
-                                renderIcon={Edit}
-                                iconDescription="Editar Domicilio"
-                                onClick={() => {handleBatchActionClickUpdateAddress(selectedRows)}}>
-                                Editar Domicilio
-                            </TableBatchAction>
-                        </TableBatchActions>
-                        <TableToolbarContent>
-                            <TableToolbarSearch onChange={onInputChange} placeholder="Buscar cliente" />
-                        </TableToolbarContent>
-                    </TableToolbar>
-                    <Table {...getTableProps()}>
-                        <TableHead>
-                            <TableRow>
-                                <TableExpandHeader />
-                                {headers.map((header, i) => (
-                                    <TableHeader key={i} {...getHeaderProps({ header })}>
-                                        {header.header}
-                                    </TableHeader>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <Fragment key={row.id}>
-                                    <TableRow {...getRowProps({ row })}>
-                                        <TableSelectRow {...getSelectionProps({ row })} />
-                                        {row.cells.map((cell) => (
-                                            <TableCell key={cell.id}>{cell.value}</TableCell>
+            {showNotification ?
+                <InlineNotification
+                    kind={notificationInfo.kind || "error"}
+                    title={notificationInfo.title || ""}
+                    style={{ marginBottom: "2rem" }}
+                    actions={
+                        <NotificationActionButton
+                            onClick={() => { window.location.reload() }}
+                        >
+                            Recargar
+                        </NotificationActionButton>
+                    }
+                >
+                </InlineNotification>
+
+                :
+
+                <></>
+            }
+            <DataTable
+                isSortable
+                rows={props.clients.slice(itemBegin, itemEnd)}
+                headers={headers}
+                {...props}
+                render={({
+                    rows,
+                    headers,
+                    getHeaderProps,
+                    getSelectionProps,
+                    getToolbarProps,
+                    getBatchActionProps,
+                    getRowProps,
+                    onInputChange,
+                    selectedRows,
+                    getTableProps,
+                    getTableContainerProps,
+                }) => (
+                    <>
+                        <TableContainer
+                            title="Clientes Registrados"
+                            description="Selecciona un cliente para modificar su estado de actividad o domicilio."
+                            {...getTableContainerProps()}>
+                            <TableToolbar {...getToolbarProps()}>
+                                <TableBatchActions {...getBatchActionProps()} translateWithId={customTranslationForTableBatchActions}>
+                                    <TableBatchAction
+                                        renderIcon={UserActivity}
+                                        iconDescription="Cambiar Estado de Actividad"
+                                        onClick={() => { handleBatchActionClickUpdateStatus(selectedRows) }}>
+                                        Cambiar Estado de Actividad
+                                    </TableBatchAction>
+                                    <TableBatchAction
+                                        renderIcon={Edit}
+                                        iconDescription="Editar Domicilio"
+                                        onClick={() => { handleBatchActionClickUpdateAddress(selectedRows) }}>
+                                        Editar Domicilio
+                                    </TableBatchAction>
+                                </TableBatchActions>
+                                <TableToolbarContent>
+                                    <TableToolbarSearch onChange={onInputChange} placeholder="Buscar cliente" />
+                                </TableToolbarContent>
+                            </TableToolbar>
+                            <Table {...getTableProps()}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableExpandHeader />
+                                        {headers.map((header, i) => (
+                                            <TableHeader key={i} {...getHeaderProps({ header })}>
+                                                {header.header}
+                                            </TableHeader>
                                         ))}
                                     </TableRow>
-                                </Fragment>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            )}
-        />
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map((row) => (
+                                        <Fragment key={row.id}>
+                                            <TableRow {...getRowProps({ row })}>
+                                                <TableSelectRow {...getSelectionProps({ row })} />
+                                                {row.cells.map((cell) => (
+                                                    <TableCell key={cell.id}>{cell.value}</TableCell>
+                                                ))}
+                                            </TableRow>
+                                        </Fragment>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                            <Pagination
+                                backwardText="Anterior"
+                                forwardText="Siguiente"
+                                itemsPerPageText="Clientes por página:"
+                                page={1}
+                                pageNumberText="Página"
+                                pageSize={10}
+                                pageSizes={[
+                                    10,
+                                    25,
+                                    50,
+                                ]}
+                                onChange={handlePageChange}
+                                totalItems={props.clients.length}
+                            />
+                        </TableContainer>
+                        <br /><br /><br />
+                    </>
+                )}
+            />
         </>
     );
 
