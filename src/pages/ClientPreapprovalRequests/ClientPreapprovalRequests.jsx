ce5ea834cebdb749c47036dbe28a152b0520e411
Santiago Valle <snvc00@gmail.com>
2021-06-17 00:11:38 -0500

    Migrate to arrow component, fetch requests inside of useEffect, display table based on role

--- src/pages/ClientPreapprovalRequests/ClientPreapprovalRequests.jsx
+++ src/pages/ClientPreapprovalRequests/ClientPreapprovalRequests.jsx
@@ -2,150 +2,115 @@ import {
     Grid,
     Row,
     Column,
-    TextInput,
+    InlineNotification,
     ComposedModal,
     ModalHeader,
     ModalBody,
     ModalFooter,
-    ToastNotification,
-    Link,
 } from "carbon-components-react";
 
 import Header from "../../components/Header";
 import MainHeading from "../../components/MainHeading";
-import PreapprovalRequestsTable from "../../components/PreapprovalRequestsTable";
+import PreapprovalRequestsClientTable from "../../components/PreapprovalRequestsClientTable";
 import PreapprovalRequestsTableSkeleton from "../../components/PreapprovalRequestsTableSkeleton";
-import { Component, createRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { Helmet } from "react-helmet";
+import { AuthContext } from "../../Auth";
 
 const gridStyles = {
     marginTop: "80px",
 }
 
-class ClientPreapprovalRequests extends Component {
-    constructor(props) {
-        super(props);
+const ClientPreapprovalRequests = () => {
+    const [preapprovals, setPreapprovals] = useState([]);
+    const [showNotification, setShowNotification] = useState(false);
+    const [notificationInfo, setNotificationInfo] = useState();
 
-        this.state = {
-            curpIsValid: true,
-            isModalOpen: true,
-            requests: undefined,
-            isAuthenticated: undefined,
-        }
+    const { currentUser, userRole } = useContext(AuthContext);
 
-        this.isCurpValid = this.isCurpValid.bind(this);
-        this.handleModalSubmit = this.handleModalSubmit.bind(this);
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACKEND_API}/api/clients/${currentUser.email}/preapproval_requests/`, {
+            method: "GET",
+            headers: {
+                "Token": sessionStorage.getItem("token")
+            }
+        })
+            .then(async (response) => ({ data: await response.json(), responseOk: response.ok }))
+            .then(({ data, responseOk }) => {
+                if (responseOk) {
+                    const preapprovals = formatPreapprovals(data);
+                    setPreapprovals(preapprovals);
+                }
+                else {
+                    throw data.detail;
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                setNotificationInfo({
+                    kind: "error",
+                    title: error,
+                });
+                setShowNotification(true);
+            });
+    }, []);
 
-        this.curpRef = createRef();
-        this.passwordRef = createRef();
-    }
-
-    handleModalSubmit() {
-        const curp = this.curpRef.current.value;
-        const password = this.passwordRef.current.value;
-
-        console.log(curp, password);
-
-        this.setState({ isModalOpen: false });
-    }
-
-    isCurpValid() {
-        var curpHasRequiredLength = false;
-
-        if (typeof (this.curpRef.current.value) === "string") {
-            curpHasRequiredLength = this.curpRef.current.value.length === 18;
-        }
-
-        this.setState({
-            curpIsValid: curpHasRequiredLength,
+    const formatPreapprovals = (preapprovals) => {
+        const formattedPreapprovals = preapprovals.map(preapproval => {
+            return {
+                id: Number(preapproval.id).toString(),
+                approved: preapproval.approved ? "Si" : "No",
+                active : preapproval.active ? "Si" : "No",
+                creation_timestamp: preapproval.creation_timestamp,
+                last_update: preapproval.last_update,
+                credit_card_name: preapproval.credit_card.name,
+                credit_card_max_credit: `$${preapproval.credit_card.max_credit} MXN`,
+                reviewed_by: preapproval.reviewed_by ? preapproval.reviewed_by : "",
+                credit_card_image: preapproval.credit_card.image,
+            }
         });
+
+        return formattedPreapprovals;
     }
 
-    render() {
-        return (
-            <>
-                <Helmet>
-                    <title>Banco Nacional | Mis Solicitudes</title>
-                </Helmet>
-                <Header />
-                <Grid style={gridStyles}>
-                    <Row>
-                        <Column>
-                            <ComposedModal
-                                open={this.state.isModalOpen}
-                                onClose={() => {
-                                    this.setState({ isAuthenticated: false });
-                                }}
+    return (
+        <>
+            <Helmet>
+                <title>Banco Nacional | Mis Solicitudes</title>
+            </Helmet>
+            <Header />
+            <Grid style={gridStyles}>
+                <Row>
+                    <Column>
+                        {showNotification ?
+                            <InlineNotification
+                                kind={notificationInfo.kind || "error"}
+                                title={notificationInfo.title || ""}
+                                style={{ marginBottom: "2rem" }}
                             >
-                                <ModalHeader label="Identificación" title="Ingresa tus credenciales" />
-                                <ModalBody>
-                                    <p style={{ marginBottom: '1rem' }}>
-                                        Tu CURP y contraseña hace posible el poder verficar que solo tu puedes
-                                        ver tu historial de solicitudes.
-                                    </p>
-                                    <TextInput
-                                        data-modal-primary-focus
-                                        id="curp"
-                                        placeholder="CURP"
-                                        ref={this.curpRef}
-                                        style={{ marginBottom: '1rem' }}
-                                    />
-                                    <TextInput
-                                        data-modal-primary-focus
-                                        id="password"
-                                        placeholder="Contraseña"
-                                        ref={this.passwordRef}
-                                        style={{ marginBottom: '1rem' }}
-                                        type="password"
-                                    />
-                                </ModalBody>
-                                <ModalFooter 
-                                    primaryButtonText="Ingresar" 
-                                    secondaryButtonText="Cancelar" 
-                                    onRequestSubmit={() => { this.handleModalSubmit(); }} 
-                                />
-                            </ComposedModal>
-                            <MainHeading>Mis Solicitudes</MainHeading>
-                            {this.state.isAuthenticated === false ? (
-                                <Row>
-                                    <Column sm={2} md={{ span: 4, offset: 6 }}>
-                                        <ToastNotification
-                                            title="Error de Autenticación"
-                                            subtitle="Credenciales no válidas"
-                                            onCloseButtonClick={() => {
-                                                setTimeout(() => {
-                                                    window.location.reload();
-                                                }, 1000);
-                                            }}
-                                            style={{ marginBottom: "2rem" }}
-                                            caption={
-                                                <Link onClick={() => { window.location.reload(); }}>
-                                                    Intentar de nuevo
-                                                </Link>
-                                            }
-                                        >
-                                        </ToastNotification>
-                                    </Column>
-                                </Row>
-                            ) : (
-                                <></>
-                            )}
-                            {
-                                (this.state.requests !== undefined) ?
+                            </InlineNotification>
 
-                                    <PreapprovalRequestsTable />
+                            :
 
-                                    :
+                            <></>
+                        }
+                        <MainHeading>Mis Solicitudes</MainHeading>
+                        {
+                            userRole === "client" ?
 
-                                    <PreapprovalRequestsTableSkeleton />
-                            }
-                        </Column>
-                    </Row>
-                </Grid>
-            </>
-        );
-    }
+                                <PreapprovalRequestsClientTable preapprovals={preapprovals} clientName={currentUser.displayName} />
+
+                                :
+
+                                <PreapprovalRequestsTableSkeleton />
+                        }
+                    </Column>
+                </Row>
+            </Grid>
+        </>
+    );
+
 }
 
 export default ClientPreapprovalRequests;
