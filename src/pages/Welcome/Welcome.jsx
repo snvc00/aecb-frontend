30e931b249ab0db85d187bec715f6cc19fc7a15f
Santiago Valle <snvc00@gmail.com>
2021-06-17 00:02:00 -0500

    Add notifications, update from benefits to promotions

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -5,6 +5,8 @@ import {
     Column,
     SkeletonPlaceholder,
     Link,
+    InlineNotification,
+    NotificationActionButton,
 } from "carbon-components-react";
 import {
     Purchase24,
@@ -13,63 +15,91 @@ import {
 } from "@carbon/icons-react";
 import { Helmet } from 'react-helmet'
 import MainHeading from "../../components/MainHeading";
+import { AuthContext } from "../../Auth";
+import { useContext, useEffect, useState } from "react";
 import "./Welcome.css";
+import { useHistory } from "react-router-dom";
 
-const Welcome = () => (
-    <>
-        <Helmet>
-            <title>Banco Nacional | Bienvenido</title>
-        </Helmet>
-        <Header />
-        <Grid className="spaced top">
-            <Row>
-                <Column>
-                    <MainHeading>Banco Nacional desde 2021</MainHeading>
-                    <SkeletonPlaceholder style={{ width: "100%", height: 400 }} />
-                </Column>
-            </Row>
-        </Grid>
-        <Grid className="spaced">
-            <Row>
-                <Column>
-                    <h4><strong>Servicios a Clientes</strong></h4><br />
-                    <p className="spaced">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <Link href="/preaprobacion" renderIcon={Purchase24} size="lg">
-                        Solicitar Tarjeta
-                    </Link>
-                </Column>
-                <Column>
-                    <h4><strong>Servicios a Administradores</strong></h4><br />
-                    <p className="spaced">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <Link href="/clientes" renderIcon={Group24} size="lg">
-                        Gestionar Clientes
-                    </Link>
-                </Column>
-                <Column>
-                    <h4><strong>Beneficios Banco Nacional</strong></h4><br />
-                    <p className="spaced">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <Link href="/beneficios" renderIcon={Badge24} size="lg">
-                        Ver Beneficios
-                    </Link>
-                </Column>
-            </Row>
-        </Grid>
-    </>
-);
+const Welcome = () => {
+    const { currentUser, errorMessage } = useContext(AuthContext);
+    const [showNotification, setShowNotification] = useState(true);
+    let history = useHistory();
+
+    return (
+        <>
+            <Helmet>
+                <title>Banco Nacional | Bienvenido</title>
+            </Helmet>
+            <Header />
+            <Grid className="spaced top">
+                <Row>
+                    <Column>
+                        {
+                            (errorMessage && showNotification) ?
+                                <InlineNotification
+                                    kind="error"
+                                    title={errorMessage}
+                                    onCloseButtonClick={() => { setShowNotification(false); }}
+                                    actions={
+                                        <NotificationActionButton
+                                            onClick={() => { history.push("/login") }}
+                                        >
+                                            Reintentar
+                                        </NotificationActionButton>
+                                    }
+                                />
+
+                                :
+
+                                <></>
+                        }
+                        <MainHeading>Bienvenido <span style={{textTransform: "capitalize"}}>{currentUser ? currentUser.displayName.toLowerCase() : "a Banco Nacional"}</span></MainHeading>
+                        <SkeletonPlaceholder style={{ width: "100%", height: 400 }} />
+                    </Column>
+                </Row>
+            </Grid>
+            <Grid className="spaced">
+                <Row>
+                    <Column>
+                        <h4><strong>Servicios a Clientes</strong></h4><br />
+                        <p className="spaced">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <Link href="/preaprobacion" renderIcon={Purchase24} size="lg">
+                            Solicitar Tarjeta
+                        </Link>
+                    </Column>
+                    <Column>
+                        <h4><strong>Servicios a Administradores</strong></h4><br />
+                        <p className="spaced">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <Link href="/clientes" renderIcon={Group24} size="lg">
+                            Gestionar Clientes
+                        </Link>
+                    </Column>
+                    <Column>
+                        <h4><strong>Promociones en Tarjetas de Crédito</strong></h4><br />
+                        <p className="spaced">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <Link href="/promociones-tarjetas" renderIcon={Badge24} size="lg">
+                            Ver Promociones
+                        </Link>
+                    </Column>
+                </Row>
+            </Grid>
+        </>
+    );
+}
 
 export default Welcome;
